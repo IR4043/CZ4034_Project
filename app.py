1bda51e26edf7db0a98ded727059535867cab228
richiepui <richiepui@gmail.com>
2023-04-08 21:21:59 +0800

    Updated API call to handle search terms.

--- app.py
+++ app.py
@@ -47,39 +47,56 @@ def suggest_terms(search_term):
     return terms
 
 
-@app.route('/facet_query', methods=["POST"])
-def facet_query():
+@app.route('/general_query', methods=["POST"])
+def general_query():
     data_list = request.get_json()
     facet_fields = {
         "size": data_list["size"],
-        "color": data_list["color"],
-        "critic": data_list["critic"],
+        "critic": data_list["critic"]
     }
-    if len(data_list["service_provider"]) > 1:
-        format_service = "%22" + quote(data_list["service_provider"]) + "%22"
-        facet_fields["service_provider"] = format_service
+    # Declare Variables
+    format_service = ""
+    format_product = ""
+    format_color = ""
+
+    # Pre-Processing for category fields
+    if data_list["service_provider"]:
+        format_service = quote(data_list["service_provider"])
+        format_service = "%22" + format_service + "%22"
+    if data_list["product_grade"]:
+        format_product = quote(data_list["product_grade"])
+        format_product = "%22" + format_product + "%22"
+    if data_list["color"]:
+        format_color = quote(data_list["color"])
+        format_color = "%22" + format_color + "%22"
+
+    # Adding it to the facet fields dictionary
+    facet_fields["service_provider"] = format_service
+    facet_fields["product_grade"] = format_product
+    facet_fields["color"] = format_color
+
+    base_query = "http://localhost:8983/solr/amazon_iphone/select?defType=dismax&q="
+    if data_list["search_term"]:
+        format_search_term = quote(data_list["search_term"])
+        base_query += format_search_term + "&mm=1&"
+        # Assigning importance to reviewDescription
+        base_query += "qf=reviewDescription&"
     else:
-        facet_fields["service_provider"] = data_list["service_provider"]
+        base_query += "*:*&"
 
-    if len(data_list["product_grade"]) > 1:
-        format_product = "%22" + quote(data_list["product_grade"]) + "%22"
-        facet_fields["product_grade"] = format_product
-    else:
-        facet_fields["product_grade"] = data_list["product_grade"]
-
-    base_query = "http://localhost:8983/solr/amazon_iphone/select?q=*:*&"
-    facet_q = "facet.field"
-
-    if any(facet_fields.values()):
-        base_query += "facet=true&"
-
+    count = 0
     for field, value in facet_fields.items():
-        if value:
-            base_query += f"{facet_q}={field}&fq={field}:{value}&"
+        if value and count == 0:
+            base_query += f"fq={field}:{value} AND "
+            count += 1
+        elif value:
+            base_query += f"{field}:{value} AND "
 
+    base_query = base_query.rstrip(' AND ')
+
+    # Adding Page Numbers
     page_number = (data_list["page"] - 1) * 9
-
-    base_query += f"start={page_number}&rows=9"
+    base_query += f"&start={page_number}&rows=9"
 
     headers = {"Content-Type": "application/json"}
     start = time.time()
@@ -89,6 +106,20 @@ def facet_query():
     result = result.json()
     result.update(time_taken)
     return result
+
+# facet_q = "facet.field"
+    #
+    # if any(facet_fields.values()):
+    #     base_query += "facet=true&"
+    #
+    # for field, value in facet_fields.items():
+    #     if value:
+    #         base_query += f"{facet_q}={field}&"
+    #
+    # for field, value in facet_fields.items():
+    #     if value:
+    #         base_query += f"fq={field}:{value} AND "
+    #
 
 
 @app.route('/mlt_query', methods=["POST"])
